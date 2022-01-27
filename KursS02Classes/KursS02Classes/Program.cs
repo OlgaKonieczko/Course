fda6dc0733dd6830d84a52e956dbdf32807d495a
Olga Konieczko <konieczko.olga@gmail.com>
2022-01-27 18:51:32 +0100

    Podsumowanie klas część 1

---
+++ KursS02Classes/KursS02Classes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace KursS02Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"To start press 's', to pause press 'p', press any other key to stop");
+
+            string start = "s";
+            string pause = "p";
+            var stopwatch = new Stopwatch();
+            bool end = false;
+
+
+            while (end == false)
+
+            {
+                string help = Console.ReadLine();
+
+                if (help == start || help == start.ToLower())
+                {
+                    stopwatch.Start();
+                }
+
+                else if (help == pause || help == pause.ToLower())
+                {
+                    stopwatch.Stop();
+                    Console.WriteLine(stopwatch.Stop().ToString(@"mm\:ss\.fff"));
+                }
+
+                else
+                {
+                    end = true;
+                    break;
+                }
+
+            }
+        }
+
+    }
+}
+
---
+++ KursS02Classes/KursS02Classes/Stopwatch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace KursS02Classes
+{
+    public class Stopwatch
+
+    {
+        private DateTime _startTime = new DateTime();
+        private DateTime _endTime = new DateTime();
+        private TimeSpan _timeResult = new TimeSpan();
+        private bool _isStarted;
+
+        public bool IsStarted { get => _isStarted; set => _isStarted = value; }
+        
+
+        public void Start()
+        {
+            if (IsStarted == false)
+            {
+                _startTime = DateTime.Now;
+                IsStarted = true;
+            }
+
+            else
+            {
+                throw new InvalidOperationException("Zatrzymaj program");
+            }
+        }
+
+        public TimeSpan Stop()
+        {
+            _endTime = DateTime.Now;
+            _timeResult = (_endTime - _startTime);
+            IsStarted = false;
+
+            return _timeResult;
+        }
+    }
+}
